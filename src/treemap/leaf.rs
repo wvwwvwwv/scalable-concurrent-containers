7affceddcb6743a1f521abe33796a1611203f739
Changgyoo Park <wvwwvwwv@me.com>
2020-11-29 21:58:43 +0900

    feature(treemap): Leaf APIs

--- src/treemap/leaf.rs
+++ src/treemap/leaf.rs
@@ -1,32 +1,61 @@
 use crossbeam_epoch::Atomic;
 use std::mem::MaybeUninit;
+use std::sync::atomic::AtomicU32;
 
-pub const ARRAY_SIZE: usize = 8;
+pub const ARRAY_SIZE: usize = 7;
 
-/// Each entry has four states: initial = 0, locked = 1, occupied = 2, removed = 3
-const INITIAL: u64 = 0;
-const LOCKED: u64 = 1;
-const OCCUPIED: u64 = 2;
-const REMOVED: u64 = 3;
-const STATE_MASK: u64 = (1u64 << ARRAY_SIZE * 2) - 1;
-
-/// Ench entry is assigned a rank
-const RANK_MASK: u64 = ((1u64 << ARRAY_SIZE * 4) - 1) << (ARRAY_SIZE * 2);
-
-/// Reserved
-const RESERVED: u64 = ((1u64 << (ARRAY_SIZE * 2)) - 1) << (ARRAY_SIZE * 6);
+/// Metadata layout: invalidated: 1-bit | max-key removed: 1-bit | rank-index map: 21-bit | occupancy: 7-bit
+///
+/// State interpretation
+///  - !OCCUPIED && RANK = 0: initial state
+///  - OCCUPIED && RANK = 0: locked
+///  - OCCUPIED && RANK > 0: inserted
+///  - !OCCUPIED && RANK > 0: removed
+const RANK_INDEX_ENTRY_SIZE: usize = 3;
+const RANK_INDEX_MAP_MASK: u32 = ((1u32 << (ARRAY_SIZE * RANK_INDEX_ENTRY_SIZE)) - 1) << ARRAY_SIZE;
+const RANK_INDEX_ENTRY_MASK: u32 = ((1u32 << RANK_INDEX_ENTRY_SIZE) - 1) << ARRAY_SIZE;
+const OCCUPANCY_MASK: u32 = (1u32 << ARRAY_SIZE) - 1;
+const OCCUPANCY_BIT: u32 = 1;
+const MAX_KEY_REMOVED: u32 = 1u32 << ARRAY_SIZE * (RANK_INDEX_ENTRY_SIZE + 1);
+const INVALIDATED: u32 = MAX_KEY_REMOVED << 1;
 
 pub type EntryArray<K, V> = [MaybeUninit<(K, V)>; ARRAY_SIZE];
 
 /// Leaf stores key-value pairs.
 pub struct Leaf<K: Clone + Ord + Sync, V: Clone + Sync> {
-    metadata: u64,
+    max_key_entry: (K, V),
     entry_array: EntryArray<K, V>,
+    metadata: AtomicU32,
     next: Atomic<Leaf<K, V>>,
 }
 
 impl<K: Clone + Ord + Sync, V: Clone + Sync> Leaf<K, V> {
-    pub fn new(capacity: usize) {}
+    pub fn new(max_key: K, value: V) -> Leaf<K, V> {
+        Leaf {
+            max_key_entry: (max_key, value),
+            entry_array: unsafe { MaybeUninit::uninit().assume_init() },
+            metadata: AtomicU32::new(0),
+            next: Atomic::null(),
+        }
+    }
+
+    pub fn insert(key: K, value: V) -> Option<(K, V)> {
+        Some((key, value))
+    }
+
+    pub fn remove(key: &K) -> Option<V> {
+        None
+    }
+
+    pub fn search(key: &K) -> Option<&V> {
+        None
+    }
+
+    pub fn invalidate() {}
+}
+
+impl<K: Clone + Ord + Sync, V: Clone + Sync> Drop for Leaf<K, V> {
+    fn drop(&mut self) {}
 }
 
 #[cfg(test)]
@@ -37,7 +66,27 @@ mod test {
 
     #[test]
     fn static_assertions() {
-        assert_eq!(RESERVED | RANK_MASK | STATE_MASK, !0u64);
+        assert_eq!(MAX_KEY_REMOVED & OCCUPANCY_MASK, 0);
+        assert_eq!(MAX_KEY_REMOVED & RANK_INDEX_MAP_MASK, 0);
+        assert_eq!(RANK_INDEX_MAP_MASK & OCCUPANCY_MASK, 0);
+        assert_eq!(
+            RANK_INDEX_MAP_MASK & RANK_INDEX_ENTRY_MASK,
+            RANK_INDEX_ENTRY_MASK
+        );
+        assert_eq!(OCCUPANCY_MASK & OCCUPANCY_BIT, OCCUPANCY_BIT);
+    }
+
+    #[test]
+    fn modification() {
+        let num_threads = (ARRAY_SIZE + 1) as usize;
+        let barrier = Arc::new(Barrier::new(num_threads));
+        let mut thread_handles = Vec::with_capacity(num_threads);
+        for tid in 0..num_threads {
+            let barrier_copied = barrier.clone();
+            thread_handles.push(thread::spawn(move || {
+                barrier_copied.wait();
+            }));
+        }
     }
 
     #[test]
