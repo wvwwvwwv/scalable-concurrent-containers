b95666ed71a2c9a105142c6ba413deb6fa7842f6
Changgyoo Park <wvwwvwwv@me.com>
2024-08-12 21:28:57 +0200

    chore(*): fix lint errors

--- src/queue.rs
+++ src/queue.rs
@@ -4,7 +4,7 @@ use super::ebr::{AtomicShared, Guard, Ptr, Shared, Tag};
 use super::linked_list::{Entry, LinkedList};
 use std::fmt::{self, Debug};
 use std::iter::FusedIterator;
-use std::sync::atomic::Ordering::{AcqRel, Acquire, Relaxed, Release};
+use std::sync::atomic::Ordering::{AcqRel, Acquire, Relaxed};
 
 /// [`Queue`] is a lock-free concurrent first-in-first-out container.
 pub struct Queue<T> {
